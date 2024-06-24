1a29cc28a17d03a66023d9b21d1244f74d595b51
Hafiz655 <Hafizzohaib655@gmail.com>
2024-06-24 11:22:01 +0500

    Initial commit

---
+++ conn.php
@@ -0,0 +1,19 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "reports";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password , $database);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } else {
+//     echo "Connected successfully";
+// }
+?>
+
+
